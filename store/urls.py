3c21383d1e7f97ff4cb5674bf8b17fda59955627
cloneyjay <jamesangatia445@gmail.com>
2025-03-11 02:35:42 +0300

    reg-page, category-page, product-page

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('about/', views.about, name = 'about'),
     path('login/', views.login_user, name = 'login'),
     path('logout/', views.logout_user, name = 'logout'),
+    path('register/', views.register_user, name = 'register'),
+    path('product/<int:pk>/', views.product_detail, name = 'product'),
+    path('category/<str:categoryname>/', views.category, name = 'category'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import RegisterForm
 
 def home(request):
     products = Product.objects.all()
@@ -10,6 +14,11 @@ def home(request):
 
 def about(request):
     return render(request,'store/about.html', {})
+
+def product_detail(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
+
 
 def login_user(request):
     if request.method == "POST":
@@ -29,4 +38,38 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     messages.success(request, "You are logged out")
-    return redirect('home')
+    return redirect('store:home')
+
+def register_user(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, "Successfull Registration.You are now logged in")
+            return redirect('store:home')
+        else:
+            messages.success(request, "There was a problem registering your account")
+            return redirect('store:register')
+    else:
+        return render(request,'store/register.html', {'form': form})
+
+def category(request, categoryname):
+    #Replace - with space
+    categoryname = categoryname.replace('-', ' ')
+    try:
+        #Get the category
+        category = Category.objects.get(name=categoryname)
+        #Get all products in the category
+        products = Product.objects.filter(category=category)
+        return render(request, 'store/category.html', {'products': products, 'category': category})
+    except:
+        messages.error(request, "Category does not exist")
+        return redirect('store:home')
+
+    
